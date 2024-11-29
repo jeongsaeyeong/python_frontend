5091f56213a0d67f523be5d8e2cac6a1ee030bbc
jeongsaeyeong <ooooo0516@naver.com>
2024-11-29 16:00:03 +0900

    main 작업

--- src/components/Section/Main.jsx
+++ src/components/Section/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sample from '../../assets/img/sample.png'
+import HartBin from '../../assets/img/hart_bin.svg'
 
 const Main = () => {
     return (
@@ -9,7 +10,55 @@ const Main = () => {
                 <div className="list">
                     <img src={Sample} alt="" />
                     <div className='content'>
-                        
+                        <div className="top">
+                            <div className="header">
+                                <h4>가게 이름</h4>
+                                <img src={HartBin} alt="" />
+                            </div>
+                            <p className="introduce">
+                                가게 설명
+                            </p>
+                        </div>
+                        <div className="tage">
+                            <p>#메뉴1</p>
+                            <p>#메뉴2</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="list">
+                    <img src={Sample} alt="" />
+                    <div className='content'>
+                        <div className="top">
+                            <div className="header">
+                                <h4>가게 이름</h4>
+                                <img src={HartBin} alt="" />
+                            </div>
+                            <p className="introduce">
+                                가게 설명
+                            </p>
+                        </div>
+                        <div className="tage">
+                            <p>#메뉴1</p>
+                            <p>#메뉴2</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="list">
+                    <img src={Sample} alt="" />
+                    <div className='content'>
+                        <div className="top">
+                            <div className="header">
+                                <h4>가게 이름</h4>
+                                <img src={HartBin} alt="" />
+                            </div>
+                            <p className="introduce">
+                                가게 설명
+                            </p>
+                        </div>
+                        <div className="tage">
+                            <p>#메뉴1</p>
+                            <p>#메뉴2</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Section/Nav.jsx
+++ src/components/Section/Nav.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import HartBin from '../../assets/img/hart_bin.svg'
+import Homefull from '../../assets/img/home_full.svg'
+import SearchBin from '../../assets/img/search_bin.svg'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <div className='Nav_wrap'></div>
+        <div className='Nav_wrap'>
+            <div>
+                <Link to='/'><img src={HartBin} alt="" /></Link>
+                <Link to='/'><img src={Homefull} alt="" /></Link>
+                <Link to='/'><img src={SearchBin} alt="" /></Link>
+            </div>
+        </div>
     )
 }
 
